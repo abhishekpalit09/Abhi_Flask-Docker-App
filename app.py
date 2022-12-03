097d25a44603991d3c3c591b23c6468fe3b0139a
abhishekpalit09 <45020007+abhishekpalit09@users.noreply.github.com>
2022-12-03 20:05:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
